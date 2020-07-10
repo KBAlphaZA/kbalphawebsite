dde06d24bb01cd3fc0441670112fde78a0535141
Bonga Ngcobo <Bonga.Developer@outlook.com>
2020-07-10 21:50:49 +0200

    Added a contact form

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,9 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   template: `
-  <p>
-    footer works !
-    </p>
+  <footer class="footer">
+    <div class="container">
+    <div class="content has-text-centered">
+      <p>
+        Made with <3 by <a href="#">KB Alpha from The Trenches</a>
+      </p>
+    </div>
+    </div>
+    </footer>
   `,
   styles:[]
 })
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,8 +2,58 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  template: `
+  <nav class="navbar" role="navigation" aria-label="main navigation" >
+  <div class="navbar-brand">
+    <a class="navbar-item" href="#">
+      <img src="/assets/icons8-alpha-100.png" width="50" height="100"/>
+    </a>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-end">
+    <div class="navbar-start">
+      <a class="navbar-item" routerLink="" routerLinkActive="active">
+        Home
+      </a>
+
+      <a class="navbar-item" routerLink="" routerLinkActive="active">
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            Services
+          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item" routerLink="/softwaredevservice" routerLinkActive="active">
+              Software development
+            </a>
+            <a class="navbar-item" routerLink="/serapismedical" routerLinkActive="active">
+              Serapis Medical
+            </a>
+          </div>
+
+      </div>
+      </a>
+
+      <a class="navbar-item" routerLink="/about-us" routerLinkActive="active">
+        Contact
+      </a>
+      
+      <a class="navbar-item" routerLink="/about-us" routerLinkActive="active">
+        About
+      </a>
+
+    </div>
+  </div>
+
+</nav >
+  `,
+styles:[]
 })
 export class HeaderComponent implements OnInit {
 
