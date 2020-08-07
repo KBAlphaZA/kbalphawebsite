8a1b6c5b42afc969d7772d1b1bd657ed3462c6ad
KhanyiTheGreat <khanyisani.buthelezi03@gmail.com>
2020-08-07 18:50:42 +0200

    js file added

---
+++ src/app/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: 'mongodb+srv://coFounderKhanyi:earlsweatshirt03@cluster0.kkaq0.azure.mongodb.net/test?authSource=admin&replicaSet=atlas-92btbu-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true',
+  secret: ''
+}
