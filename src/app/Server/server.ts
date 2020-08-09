cbfff11c093dc2768e195ad71015065ab16b2a6f
KhanyiTheGreat <khanyisani.buthelezi03@gmail.com>
2020-08-09 18:23:09 +0200

    test

--- src/app/Server/server.ts
+++ src/app/Server/server.ts
@@ -5,14 +5,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const port = 4200;
 
-const database = mongoose.connect('http://localhost:4200', (err, response) => {
+
+/* Mongo connection
+  const database = mongoose.connect('http://localhost:4200', (err, response) => {
   if (err){console.log(err); }
   else{console.log('Connected to' + database + ' ' + response); }
-});
 
-const app = express();
 
-app.use(bodyParser());
+  app.use(bodyParser());
 app.use(bodyParser.json({limit: '7mb'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -27,3 +27,20 @@ app.use((req, res, next) => {
 let schema = mongoose.Schema;
 
 app.post()
+});
+*/
+
+
+const app = express();
+
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist/kbalphawebsite'));
+
+app.get('/*', (req, res) =>
+{
+    res.sendFile(path.join(__dirname + '/dist/kbalphawebsite/index.html'));
+});
+
+
+
+app.listen(process.env.PORT || 4200)
