4c14d5ea16a93a92b5feb2b4cfeaf61299cfc4a7
Bonga Ngcobo <Bonga.Developer@outlook.com>
2020-08-16 00:09:37 +0200

    added some code to dectect screen size changes and update the UI

--- src/app/LandingPage/landingpagecomponent/landingpagecomponent.component.ts
+++ src/app/LandingPage/landingpagecomponent/landingpagecomponent.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { HostListener,Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landingpagecomponent',
   templateUrl: './landingpagecomponent.component.html',
   styleUrls: ['./landingpagecomponent.component.css']
 })
+
+
+
 export class LandingpagecomponentComponent implements OnInit {
 
   constructor() { }
-
+  public innerHeight: any;
+  public innerWidth: number;
+  //public marginBottom: number;
   ngOnInit(): void {
+    //this.innerHeight = window.innerHeight;
+    //this.innerWidth = window.innerWidth;
   }
-
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+  this.innerHeight = window.innerHeight;
+  this.innerWidth = window.innerWidth;
+  console.log(innerWidth,"x",innerHeight);
+}
+  
 }
