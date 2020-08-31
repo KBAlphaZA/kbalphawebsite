dcb86d27fa69a82ca3024418e0a36e537f560612
Bonga Ngcobo <Bonga.developer@outlook.com>
2020-08-31 22:13:08 +0200

    made major changes to the server

---
+++ server.ts
@@ -0,0 +1,50 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+//var enquiry = require("../Server/api/Controllers/Enquiry.route")
+const mongoose = require('mongoose');
+
+const app = express();
+
+//cors setup
+
+//mongodb
+const dburl =
+  'mongodb+srv://coFounderBonga:12345@cluster0.kkaq0.azure.mongodb.net/PotentialClientDb?retryWrites=true&w=majority';
+//Mongo Connection
+
+mongoose
+  .connect(dburl, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected');
+  })
+  .catch((err) => {
+    console.log('Could not connect \n :', err);
+  });
+
+app.use(cors());
+process.env.NODE_ENV !== 'prod' && app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Replace the '/dist/<to_your_project_name>'
+
+app.use(express.static('./dist/'));
+//routes
+app.use(
+  '/customerlead',
+  require('./src/app/Server/api/Controllers/EnquiryController.ts')
+);
+
+app.use(express.static('./dist/kbalphawebsite'));
+
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/kbalphawebsite' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- src/app/Server/api/Controllers/EnquiryController.ts
+++ src/app/Server/api/Controllers/EnquiryController.ts
@@ -3,56 +3,49 @@
 const express = require('express');
 const router = express.Router();
 
-
-
-router.post('/post',(req,res) =>{
+router.post('/post', (req, res) => {
   //fill in data
-  const CustomerName: req.body.name;
-  const CustomerEmail: req.body.email;
+  const CustomerName = req.body.name;
+  const CustomerEmail = req.body.email;
   const date = Date.now();
-  
+
   //FILTER
   const filter = {};
   // UPDATE
-  if(req.body == null)
-    res.send({'Error Message':'There is no data to process'})
-  
-  //VALIDATE DATA
-  CustomerLead.findOne({email: req.body.email })
-    .then(customer => {
-      if(customer)
-        res.send({
-          'reason':'Customer email already exists',
-          'result':false
-        });        
-    //example data is added below
-    //The data maps 1:1 if the names match so this eg. {Name: Name} is the same as just { Name } 
-    const newCustomer = new CustomerLead({
+  if (req.body == null)
+    res.send({ 'Error Message': 'There is no data to process' });
 
+  //VALIDATE DATA
+  CustomerLead.findOne({ email: req.body.email }).then((customer) => {
+    if (customer)
+      res.send({
+        reason: 'Customer email already exists',
+        result: false,
+      });
+    //example data is added below
+    //The data maps 1:1 if the names match so this eg. {Name: Name} is the same as just { Name }
+    const newCustomer = new CustomerLead({
       CustomerName,
-      CustomerEmail
-    });//etc
-  //RESPONSE & Database Access
+      CustomerEmail,
+    }); //etc
+    //RESPONSE & Database Access
     newCustomer
       .save()
-      .then(customer =>{
-      //show that the customer has been stored on the db
-      //page should have a UI element/refresh the page
-      res.send({
-        'result':true
-      });            
-    }).catch(err =>
-      {
+      .then((customer) => {
+        //show that the customer has been stored on the db
+        //page should have a UI element/refresh the page
+        res.send({
+          result: true,
+        });
+      })
+      .catch((err) => {
         console.log(err);
-        res.send
-        ({
-          'result':false
+        res.send({
+          result: false,
         });
       });
   });
-  
-  
-})
+});
 /**
 export class EnquiryController {
    private customerEnquiry;//: CustomerEnquiry;
