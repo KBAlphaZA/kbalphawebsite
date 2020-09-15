1c37326046f7d82a6f0de6aacfe35f9197d5b9d5
KhanyiTheGreat <khanyisani.buthelezi03@gmail.com>
2020-09-15 18:21:08 +0200

    added a new model file.

---
+++ src/app/Server/Models/CurrentProjectInformation.ts
@@ -0,0 +1,11 @@
+export class ProjectInformation{
+  developmentStage: string;
+  projectCompletion: number;
+  totalfeaturesRequested: string[];
+  featuresDone: string[];
+  platformsToBeDevelopedOn: string[];
+  industry: string;
+  projectCost: number;
+  projectNotes: string;
+  projectType: string;
+}
