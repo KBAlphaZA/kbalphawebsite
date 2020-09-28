84499753c45baeaf8bf47638a820c78447ad2203
KhanyiTheGreat <khanyisani.buthelezi03@gmail.com>
2020-09-28 02:16:59 +0200

    binding faild, reverted.

--- src/app/components/Login/Login.component.ts
+++ src/app/components/Login/Login.component.ts
@@ -1,19 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-Login',
   templateUrl: './Login.component.html',
   styleUrls: ['./Login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  LocaldbEmail = 'info@kbalpha';
+
+  LocaldbPassword = 'LiverpoolRocks';
+
+  emailTextBox = '';
+
+  passwordTextBox = '';
+
+  constructor(private localSnackBar: MatSnackBar) { }
 
   ngOnInit() {
   }
 
   navigateToAdminMain(){
 
+    console.log('in the method', this.emailTextBox);
+
+    if (this.userAuthentication(this.emailTextBox, this.emailTextBox)){
+
+        location.href = '/admin/login/adminView';
+    }
+    else{
+        this.localSnackBar.open('Not authorized', ' ' , {duration: 4000});
+    }
+
+  }
+
+  // The follwing is the temporary authentication system to the portal.
+  userAuthentication(userEmail: string, userPassword: string): boolean{
+
+    let grantacces = false;
+
+    if ( userEmail === this.LocaldbEmail && userPassword === this.LocaldbPassword){
+
+      grantacces = true;
+
+    }
+
+    return grantacces;
+
+  }
+
+  onSubmit(f: NgForm){
+    console.log(f.value);
   }
 
 }
