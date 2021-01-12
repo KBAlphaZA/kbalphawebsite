8561db13a889128938dc0d15aae874e5bf7892ce
Bonga Ngcobo <bonga.developer@outlook.com>
2021-01-12 11:07:09 +0200

    first commit of new branch

--- src/app/LandingPage/landingpagecomponent/landingpagecomponent.component.ts
+++ src/app/LandingPage/landingpagecomponent/landingpagecomponent.component.ts
@@ -25,6 +25,10 @@ export class LandingpagecomponentComponent implements OnInit {
     var element = document.getElementById("headerImage")
     this._innerHeightString = this._innerHeight.toString() + "px";
     
+    //
+    // We need to fix the box issue before implmenting the resizing solution
+    //
+
     //element.style.height = this._innerHeightString;
     
   }
